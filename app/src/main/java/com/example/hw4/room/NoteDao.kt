e6415a0b94126c00d74a261d296fe86c224eaa0b
ABgithub1 <Mcformytable@outlook.com>
2022-04-24 17:39:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hw4/room/NoteDao.kt
@@ -0,0 +1,27 @@
+package com.example.hw4.room
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.hw4.data.Note
+
+@Dao
+interface NoteDao {
+
+    @Query("SELECT * FROM note")
+    fun getAll(): List<Note>
+
+    @Query("SELECT * FROM note WHERE id IN (:noteId)")
+    fun loadAllByIds(noteId: IntArray): List<Note>
+
+    @Query("SELECT * FROM note WHERE title LIKE :title")
+    fun findByTitle(title: String): Note
+
+    @Insert
+    fun insertAll(vararg note: Note)
+
+    @Delete
+    fun delete(note: Note)
+
+}
